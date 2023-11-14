d92a0ed19a15f4238f809c3a51da13638cbbc36b
c huseby <catherine_huseby@outlook.com>
2023-11-14 09:49:12 -0600

    we added code

--- DenyReply/ViewController.swift
+++ DenyReply/ViewController.swift
@@ -6,19 +6,54 @@
 //
 
 import UIKit
+class AppData
+{
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
-//help me please god
-    
-    
     
     
     
 }
-
+class card
+{
+    var question : String
+    var answer : String
+    
+    
+    //<<<<<<< Updated upstream
+    init(question2: String, answer2: String) {
+        question = question2
+        answer = answer2
+        //=======
+        
+    }
+    
+    class cardSetClass {
+        
+        var setName = ""
+        var setColor = ""
+        var setImage = ""
+        var cards: [card] = []
+        init(setName: String, setColor: String, cards: [card]) {
+            self.setName = setName
+            self.setColor = setColor
+            self.cards = cards
+            
+            //>>>>>>> Stashed changes
+        }
+        
+        //  }
+        
+        
+    }
+    class ViewController: UIViewController {
+        
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            // Do any additional setup after loading the view.
+        }
+        //Yassss
+        //wwwwwwwwww
+        
+    }
+    
+}
