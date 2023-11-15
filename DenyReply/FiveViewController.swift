939d45c3993c30b845773a77d5137aa9a65f59fd
Corey Atkinson <catkinson2025@gmail.com>
2023-11-15 09:45:52 -0600

    add card

---
+++ DenyReply/FiveViewController.swift
@@ -0,0 +1,29 @@
+//
+//  FiveViewController.swift
+//  DenyReply
+//
+//  Created by COREY ATKINSON on 11/15/23.
+//
+
+import UIKit
+
+class FiveViewController: UIViewController {
+    @IBOutlet weak var questionOutlet: UITextField!
+    
+    @IBOutlet weak var answerOutlet: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func addCardAction(_ sender: UIButton) {
+        
+        
+        
+        
+    }
+    
+
+}
